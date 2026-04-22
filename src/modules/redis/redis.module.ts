f21e6a616134fbb68b2fa120770c5df65e48632e
mirsaid-abduqulov <mirsaidabduqulov@gmail.com>
2026-04-22 16:29:19 +0500

    redis add

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -6,7 +6,7 @@ import { RedisService } from './redis.service';
   imports: [
     IoRedisModule.forRoot({
       type: 'single',
-      url: process.env.REDIS_URL || 'redis://localhost:6379',
+      url: process.env.UPSTASH_REDIS_REST_URL || 'redis://localhost:6379',
       
     }),
   ],
